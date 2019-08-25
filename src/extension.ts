812587f0ed5e4bbc20f6c1f38b637db8be9f9f10
Jacob Lundberg <jacob@skira.se>
2019-08-25 22:47:58 +0200

    Add touch bar buttons and controlling logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "touch-bar-commands" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	const commandNo = [1,2,3,4];
+	context.subscriptions.push(...commandNo.map((no): vscode.Disposable => {
+		return vscode.commands.registerCommand(
+			`touch-bar-commands.execute.cmd${no}`,
+			() => {
+				const conf = vscode.workspace.getConfiguration();
+				vscode.commands.executeCommand('workbench.action.terminal.focus');
+				let terminal = vscode.window.activeTerminal;
+				if (!terminal) {
+					terminal = vscode.window.createTerminal({ name: 'Touch Bar Commands' });
+					terminal.show(true);
+				}
+				const cmd = conf.get<string>(`touch-bar-commands.command.${no}`);
+				terminal.sendText(cmd ? cmd : `echo "Error: no command specified for command ${no}"`);
+			}
+		);
+	}));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
